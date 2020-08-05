df4646ffcdd7356dee4f6009da22b01fd0c5e7cf
Dave Stevens <d4v3.uk@googlemail.com>
2020-08-05 20:21:06 +0100

    Switches to use async/await.

--- src/services/extractText.ts
+++ src/services/extractText.ts
@@ -12,26 +12,24 @@ const getColor = (index: number): string => {
 
 // TODO: image input
 // TODO: request from Azure and wait for response
-export default (): Promise<IResult[]> => {
-  return fetch("/azure.response.json").then((response) => {
-    if (response.ok) {
-      return new Promise<IResult[]>((resolve) => {
-        setTimeout(() => {
-          response.json().then((data) => {
-            if (data.status !== "succeeded") {
-              throw new Error(`Something broke: ${data.status}`);
-            }
-            resolve(
-              data?.analyzeResult?.readResults[0]?.lines.map((line, index) => ({
-                boundingBox: line.boundingBox,
-                color: getColor(index),
-                text: line.text
-              }))
-            );
-          });
-        }, 100);
-      });
-    }
+export default async (): Promise<IResult[]> => {
+  const response = await fetch("/azure.response.json");
+  if (!response.ok) {
     throw new Error("Something broke");
+  }
+
+  return await new Promise(async (resolve) => {
+    const json = await response.json();
+    if (json.status !== "succeeded") {
+      throw new Error(`Something broke: ${json.status}`);
+    }
+
+    resolve(
+      json?.analyzeResult?.readResults[0]?.lines.map((line, index) => ({
+        boundingBox: line.boundingBox,
+        color: getColor(index),
+        text: line.text
+      }))
+    );
   });
 };
